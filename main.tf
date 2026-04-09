38a9babaa82e2a1a067f8f9bd195fbda1de0ac1b
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-04-09 15:20:24 +0530

    added the vnet through module

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
+## Resource Group
 module "resource-group" {
     source = "git::https://github.com/narendrareddy-p/modules.git//resource-group?ref=feature"
     resource_group_name = var.rg_name
     location = var.location
   
 }
-
+##virtual Network
 module "virtual-network" {
   source = "git::https://github.com/narendrareddy-p/modules.git//virtual-network?ref=feature"
-  
+  vnet_name = var.vnet_name
+  resource_group_name = var.rg_name
+  location = var.location
+  address_space = var.address_space
+
 }
