4579d07e3b7e822e624f953ed43cdf34fe0566f1
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-04-10 10:25:27 +0530

    created a new subnet

--- main.tf
+++ main.tf
@@ -19,16 +19,8 @@ module "virtual-network" {
   resource_group_name = var.rg_name["app-team"]
   location = var.location
   address_space = var.address_space
+  subnet_name = var.subnet_name
+  address_prefixes =  var.address_prefixes
   depends_on = [ module.resource-group ]
 
-}
-
-##subnet
-module "subnets" {
-   source = "git::https://github.com/narendrareddy-p/modules.git//virtual-network?ref=feature"
-   resource_group_name = var.rg_name["app-team"]
-   subnet_name = var.subnet_name
-   vnet_name = var.vnet_name
-   address_prefixes =  var.address_prefixes
-  
 }
